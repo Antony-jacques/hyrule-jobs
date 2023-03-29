f4d5eeab7b501afb3dc6f01fcc95c9580446876f
Antony <antony.jacques@laposte.net>
2023-03-29 21:31:06 +0200

    state property type

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,39 @@
   <div class="app">
     <p>{{name}}- {{age}}</p>
     <button @click="changeName('Zelda')">change the name</button>
-    <button @click="changeAge(99)">change the age</button>
+    <button @click="changeAge('99')">change the age</button>
+    <p>{{ nameWithRef }}</p>
+    <p>{{ ageWithRef }}</p>
   </div>
   
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, reactive, toRefs, ref } from 'vue';
 
 export default defineComponent({ // defineComponent for typescript component
   name: 'App',
   components: { },
-  data(){
-    return {
+  setup(){
+    const state = reactive({
       name: 'Link',
-      age: 25 as number | string
-    }
+      age: 256 as number | string // as or reactive object
+    })
+
+    const nameWithRef = ref('John')
+    const ageWithRef = ref<number | string>(40) // <type> for ref
+
+    ageWithRef.value = '33'
+
+    return {...toRefs(state), nameWithRef, ageWithRef} 
   },
+
   methods: {
     changeName(newName: string){
       this.name = newName
       return newName
     },
-    changeAge(newAge: string | number){
+    changeAge(newAge: string | number ){
       this.age = newAge
     }
   }
